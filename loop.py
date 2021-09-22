45f2aa1db3909242b1ef2ed87e72b494e68f40d4
adriannavarro99 <adriannavarro92@gmail.com>
2021-09-21 18:59:15 -0600

    fist commit

---
+++ loop.py
@@ -0,0 +1,35 @@
+for x in range (0,151,1):
+    print(x)
+
+
+
+for multiple in  range (5, 1001,5):
+    print(multiple)
+
+
+    if x % 5 == 0:
+        print("Coding")
+    elif x % 10 == 0:
+        print("Coding Dojo")
+    else:
+        print(x)
+
+sum = 0
+for i in range(1,500001,2):
+    sum += i
+print(sum)
+
+
+for i in range(2018,-1,-4):
+    print(i)
+
+
+
+low = 2
+high = 9
+mult = 3
+
+for i in range(low,high + 1):
+    if i % mult == 0:
+        print(i)
+
